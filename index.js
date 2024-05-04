c08ce92724313776a2b2daefe735eaae7b5b76ba
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-05-04 23:36:54 +0530

    Changing port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/v1", primaryRouter)
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(3001, () => {
+    console.log("Server is running on port 3001")
 })
